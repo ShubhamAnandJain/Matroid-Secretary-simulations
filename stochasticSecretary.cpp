b110c0a382dfed5242f6b6eb9c877a1ac8b900e2
shubham <shubhamjainiitb@gmail.com>
2020-04-06 23:23:44 +0530

    Stochastic secretary has been updated to be more up-to-date with the paper "How to hire secretaries with Stochastic Departures"

--- stochasticSecretary.cpp
+++ stochasticSecretary.cpp
@@ -1,6 +1,7 @@
 #include<iostream>
 #include<random>
 #include<chrono>
+#include<queue>
 using namespace std;
 
 // mt19937_64 rng;
@@ -9,24 +10,35 @@ using namespace std;
 // seed_seq ss{uint32_t(timeSeed & 0xffffffff), uint32_t(timeSeed>>32)};
 mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
 
-bool perform_experiment(int n){
+bool perform_experiment(int n, double sample, double myu, double lambda){
 
-	// Runs one instance of the secretary problem
+	// Runs one instance of the stochastic secretary problem
 	// Returns 1 if best is hired, 0 if not
+	// Takes sample*n as the sample
 
-	poisson_distribution<int> distribution (100000);
-	uniform_real_distribution<long double> unif(0, 1); // ready to generate random numbers
+	// Arrival distribution, taken as poisson with myu as mean
 
-	long double e = exp(1);
+	poisson_distribution<int> distribution1 (myu);
 
-	int threshold = n/e;
+	// Departure distribution, taken as exponential with 1/lambda as mean
+
+	exponential_distribution<double> distribution2 (lambda);
+
+	// Distribution of skills, taken to be uniform
+
+	uniform_real_distribution<double> unif(0, 1); // ready to generate random numbers
+
+	// Actual code follows
+
+	int threshold = n*sample;
 	int maxindex = -1;
 	int holduntil = n;
-	long double maxcur = 0;
-	long double maxsample = 0;
-	bool choose = 0;
+	double maxcur = 0;
+	double maxsample = 0;
+	double maxalgo = 0;
+	int indcurr = -1;
 
-	long double generatednumbers[n];
+	double generatednumbers[n];
 
 	for(int i = 0; i < n; i++){
 		generatednumbers[i] = unif(rng);
@@ -36,17 +48,56 @@ bool perform_experiment(int n){
 		}
 	}
 
-	for(int i = 0; i < threshold; i++){
-		maxsample = max(maxsample, generatednumbers[i]);
+	priority_queue<pair<double,pair<int,int>>> events;
+
+	for(int i = 0; i < n; i++){
+		events.push(make_pair(-distribution1(rng),make_pair(1,i)));
 	}
 
-	for(int i = threshold; i < n; i++){
-		if(generatednumbers[i]>maxsample){
-			if(i == maxindex) return 1;
-			maxsample = generatednumbers[i];
-			holduntil = i + distribution(rng);
+	int left = 0;
+
+	while(!events.empty()){
+		auto curr = events.top();
+		int curindex = curr.second.second;
+		events.pop();
+
+		if(curr.second.first == 1){
+			if(generatednumbers[curindex]>maxcur){
+				maxindex = curindex;
+				maxcur = generatednumbers[maxindex];
+			}
+			double endtime = curr.first - distribution2(rng);
+			events.push(make_pair(endtime,make_pair(0,curindex)));
+			if(generatednumbers[curindex] > maxalgo){
+				maxalgo = generatednumbers[curindex];
+				indcurr = curindex;
+			}
+
+			continue;
 		}
-		if(holduntil == i) return 0;
+
+		else if(curr.second.first == 0){
+			left++;
+			if(left < threshold && curindex == maxindex){
+				return 0;
+			}
+			else if(left < threshold){
+				maxsample = max(maxsample, generatednumbers[curindex]);
+				continue;
+			}
+			if(curindex == maxindex){
+				return 1;
+			}
+			if(generatednumbers[curindex] >= maxsample && indcurr == curindex){
+				return 0;
+			}
+
+		}
+
+		else{
+			cout<<"Something went wrong\n";
+			return 0;
+		}
 	}
 
 	return 0;
@@ -56,15 +107,16 @@ bool perform_experiment(int n){
 int main()
 {
 
-	int iter = 10000; 
-	int lower = 10000;
+	int iter = 1000; 
+	int lower = 1000;
 	int upper = 10000;
-	int stepsize = 10000;
+	int stepsize = 1000;
+	double sample = 0.44;
 
 	for(int n = lower; n <= upper; n += stepsize){
 		int countsuccess = 0;
 		for(int j = 0; j < iter; j++){
-			countsuccess += (int)perform_experiment(n);
+			countsuccess += (int)perform_experiment(n,sample,10,1);
 		}
 		cout<<n<<", "<<(double)countsuccess/iter<<endl;
 	}
