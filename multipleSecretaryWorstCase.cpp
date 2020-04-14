f4da15c89ee22c86730e2adfaf131d11b0335ff3
Rohit Gurjar <rgurjar@Rohits-MacBook-Air.local>
2020-04-14 08:51:07 +0530

    added results for multiple secretary worst case

---
+++ multipleSecretaryWorstCase.cpp
@@ -0,0 +1,103 @@
+#include<iostream>
+#include<random>
+#include<algorithm>
+#include<chrono>
+#include <ctime>    // For time()
+#include <cstdlib>  // For srand() and rand()
+
+
+using namespace std;
+
+void random_perm(int ranks[], int n){
+	// random permutation
+	//int ranks[n];
+	for (int i=0; i<n; i++) ranks[i]=i+1;
+
+	//for (int i=0; i<n; i++) cout << ranks[i] << ' ';
+
+	for (int j=n; j>1; j--){
+		int r = rand()%(j);
+		swap(ranks[n-j],ranks[n-j+r]);
+	}
+}
+
+int comp_ratio(int ranks[], int n, int k, int sampleSize){
+	int topk[k]; // stores top k largest elements till now.
+	//int isTopk[n]; //denote if an element is in top k till now.
+	
+	for (int i=0; i<k; i++) topk[i]=0;
+	//for (int i=0; i<n; i++) isTopk[i]=0;
+	sort(ranks,ranks+sampleSize);	
+	int minimum = min(k, sampleSize);
+	for (int i=0; i<minimum; i++) topk[i]=ranks[sampleSize-i-1]; 
+	
+	int comp_ratio=0;
+	int curr_size=0;
+
+
+	for (int i =sampleSize; (i<n && curr_size<k); i++){
+		int curr=ranks[i];
+		int pos;
+		for (pos=0; pos<k; pos++){
+			if (topk[pos]<curr) break; 
+		}
+		for (int j= k-1; j>pos; j--){
+			topk[j]=topk[j-1];
+		}
+		if(pos<k){
+			topk[pos]=curr;
+			curr_size++;
+			if(ranks[i]>n-k) comp_ratio++;
+		}
+		
+	}
+
+
+
+	return comp_ratio;
+	
+}
+
+int main()
+{
+
+	int n = 100;
+	int iter = n*n;
+	//int k = 1;
+	//int sampleSize=7;
+
+	srand(time(0));  // Initialize random number generator.
+	int ranks[n];
+	
+	//random_perm(ranks, n);
+	
+	//for (int i=0; i<n; i++) cout << ranks[i] << ' ';
+	//cout << '\n';
+	
+	cout << "n \t k \t best_comp_ratio  best_sample_factor \n";
+	for(int k=1; k<n/2; k++){
+		double best_comp_ratio=0;
+		double bestSampleSize=0;
+		double best_sample_factor;
+		for(int sampleSize=1; sampleSize<=n-k; sampleSize++){
+			double avg_comp_ratio=0;
+			for(int i=0; i<iter; i++){
+				random_perm(ranks, n);
+				avg_comp_ratio+=comp_ratio(ranks,n,k,sampleSize);
+			}
+			avg_comp_ratio = avg_comp_ratio/iter;
+			if (avg_comp_ratio >= best_comp_ratio){
+				best_comp_ratio = avg_comp_ratio;
+				bestSampleSize=sampleSize;
+			}
+		}
+		best_sample_factor = bestSampleSize/n;
+		cout << n << " \t " << k << " \t " << best_comp_ratio/k << "\t\t" <<  bestSampleSize/n << '\n';
+	}
+	//double avg_comp_ratio=0;
+	//
+	//int comp= comp_ratio(ranks,n,k,sampleSize);
+	//cout << comp << '\n';
+
+	return 0;
+}
